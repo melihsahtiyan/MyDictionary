0d3b5a5680a4256d2cc6157424a07a7ff104a436
Melih SAHTİYAN <190706023@st.maltepe.edu.tr>
2021-01-26 15:46:23 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T, P>
+    {
+        T[] illustrators;
+        P[] items;
+
+        public MyDictionary()
+        {
+            illustrators = new T[0];
+            items = new P[0];
+        }
+
+        public void Add(T illustrator, P item)
+        {
+            T[] tempIllustrator = illustrators;
+            P[] tempItem = items;
+            illustrators = new T[illustrators.Length + 1];
+            items = new P[items.Length + 1];
+            for (int i = 0; i < tempIllustrator.Length; i++)
+            {
+                illustrators[i] = tempIllustrator[i];
+            }
+            for (int i = 0; i < tempItem.Length; i++)
+            {
+                items[i] = tempItem[i];
+            }
+            illustrators[illustrators.Length - 1] = illustrator;
+            items[items.Length - 1] = item;
+        }
+
+        public void ShowAll()
+        {
+            for (int i = 0; i < illustrators.Length; i++)
+            {
+                Console.WriteLine(illustrators[i] + " " + items[i]);
+            }
+        }
+        public void ShowRange(int a, int b)
+        {
+            if (a < b)
+            {
+                for (int i = a; i < b; i++)
+                {
+                    Console.WriteLine(illustrators[i] + " " + items[i]);
+                }
+            }
+            if (b < a)
+            {
+                for (int i = b; i < a; i++)
+                {
+                    Console.WriteLine(illustrators[i] + " " + items[i]);
+                }
+            }
+        }
+        public void ShowInitial(int a)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(illustrators[i] + " " + items[i]);
+            }
+
+
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            MyDictionary<string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("kullanıcı1", "Melih");
+            myDictionary.Add("kullanıcı2", "Engin");
+
+
+            myDictionary.ShowInitial(2);
+            myDictionary.ShowRange(1, 0);
+
+
+        }
+    }
+}
